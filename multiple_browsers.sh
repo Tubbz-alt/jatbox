79fcf5029b5525d29caaefeec4240e8ca071633c
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2015-12-08 21:05:06 -0800

    Removed unused files

--- multiple_browsers.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-parallel ::: "bin/behat -p ie --ansi --format pretty,html --out ,test_out/index.html $1 $2" "bin/behat -p firefox --ansi --format pretty,html --out ,test_out/index.html $1 $2" "bin/behat --ansi --format pretty,html --out ,test_out/index.html $1 $2"
--- parallel_behat.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p test_out
-find features/ -iname '*.feature' | parallel --gnu 'bin/behat --ansi --format pretty,html --out ,test_out/index.html {}' || exit 1
