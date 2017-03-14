9d8c2de016c8f0a999cbc213a917dbd88ca6e1e9
Benjamin Stuermer <benjamin.stuermer@pnnl.gov>
2017-03-14 15:11:45 -0700

    Converted const that was incompatible with earlier versions of PHP into a variable

--- bin/start.php
+++ bin/start.php
@@ -2,22 +2,23 @@
 <?php
     require_once (__DIR__ . '/../scripts/common.php');
 
-    const WORKING_DIR = __DIR__ . '/..';
+    // WORKING_DIR is really a constant but we define it as a variable for PHP<5.6 support.
+    $WORKING_DIR = __DIR__ . '/..';
 
     # Cleanup function that runs when this script exits. Normally this will be after the stop file (see the loop at bottom)
     # has been created to signal the script to end, but we bind it to run on exit so that the script will clean up after
     # itself if it's terminated unexpectedly
-    register_shutdown_function(function () {
+    register_shutdown_function(function () use ($WORKING_DIR){
         if ( file_exists(STOP_FILE) ) {
             unlink(STOP_FILE);
         }
 
-        chdir(WORKING_DIR);
+        chdir($WORKING_DIR);
         do_exec('vagrant halt');
         do_exec('pkill -f "selenium-server"');
     });
 
-    chdir(WORKING_DIR);
+    chdir($WORKING_DIR);
 
     # Start the VM that will run our tests
     do_exec('vagrant up');
